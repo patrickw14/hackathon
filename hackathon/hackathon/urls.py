e8af0ab1252dfa72a674c17ec94a37a168e6c375
Corbin Lewis <corbinllewis@gmail.com>
2014-11-15 03:52:15 -0800

    Added starttimeint to note model

--- hackathon/hackathon/urls.py
+++ hackathon/hackathon/urls.py
@@ -12,8 +12,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^get_notes',views.getNotes,name='getNotes'),
     url(r'^create_note/', views.createNote, name='createNote'),
-    url(r'^',views.index,name='index'),
     url(r'^get_recent_notes', views.getRecentNotes, name='getRecentNotes'),
+    url(r'^',views.index,name='index'),
 
 
 )

0770b83b6f6e6c15e4226f7d43f3c8f10a7e26ee
Marco Flowers <marco.flowers12@gmail.com>
2014-11-14 21:58:13 -0800

    first view methods

--- hackathon/geonote/views.py
+++ hackathon/geonote/views.py
@@ -3,8 +3,23 @@ from django import forms
 from django.http import HttpResponse, HttpResponseRedirect
 from models import *
 from django.contrib.auth import authenticate, login as auth_login, logout
+import json
 # Create your views here.
 
 
 def index(request):
     return HttpResponseRedirect("http://swingbyfood.com")
+
+def getNotes(request):
+	if request.method == "POST":
+		ne_lat = request.POST["ne_lat"]
+		ne_long = request.POST["ne_long"]
+		sw_lat = request.POST["sw_lat"]
+		sw_long = request.POST["sw_long"]
+		#notes = Notes.objects.filter(lat>ne_lat,lat<sw_lat,lng>ne_long,lng<sw_long)
+		# test note outputs
+		notes_output=[{"author":"hello","lat":20.11,"lng":30.11,"content":"Hola"}]
+		#notes_output=[]
+		#for note in notes:
+	#		notes_output.append({"author":note["author"],"lat":note["lat"],"lng":note["lng"],"content":note["content"]})
+		return HttpResponse(json.dumps(notes_output), content_type="application/json")
--- hackathon/hackathon/urls.py
+++ hackathon/hackathon/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from geonote import views
+
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,4 +9,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^get_notes/',views.getNotes,name='getNotes'),
+    url(r'^',views.index,name='index'),
 )

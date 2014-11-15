eb0ce9c2593a11bd93c94410c982270c290096c1
patrick.wilson <patrick.wilson@servicenow.com>
2014-11-15 00:39:01 -0800

    We're getting there

--- hackathon/geonote/static/geonote/app.geonote.js
+++ hackathon/geonote/static/geonote/app.geonote.js
@@ -1,5 +1,152 @@
-var app = angular.module("geonote", ["leaflet-directive"]);
+var app = angular.module("geonote", []);
 
-app.controller("GeonoteController", [ "$scope", function($scope) {
-	// Nothing here!
+app.factory("urlTools", function (){
+	"use strict";
+
+	function getURL(name, parameters) {
+		var url = name;
+
+		var i = 0;
+
+		angular.forEach(parameters, function(key, value) {
+			if (i == 0)
+				url += ('?' + value + '=' + key)
+			else
+				url += ('&' + value + '=' + key)
+		});
+
+		console.log(url);
+
+		return url;
+	}
+
+	return {
+		getURL: getURL,
+	};
+});
+
+app.service("notePoller", function($timeout, $http, urlTools) {
+	var notes = [];
+	var polling = false;
+
+	var poller = {
+		startPolling: function() {
+			console.log("lets start!!");
+			polling = true;
+			poll();
+		},
+
+		stopPolling: function() {
+			polling = false;
+		},
+
+		getNotes: function() {
+			return notes;
+		},
+
+		createNote: function() {
+
+		}
+	}
+
+	function poll() {
+		if (!polling) return;
+
+		var url = urlTools.getURL('/get_notes/', {
+			ne_lat: 5.0,
+			ne_long: 5.0,
+			sw_lat: 5.0,
+			sw_long: 5.0
+		});
+
+		$.ajax({
+		  url: url,
+		  success: success,
+		  dataType: 'json'
+		});
+
+		$timeout(poll, 2000);
+	}
+
+	function success(data) {
+		console.log(data);
+	}
+
+	return poller;
+})
+
+app.service("geoLocation", function() {
+	var lat = "0";
+	var lng = "0";
+	var locationReady = false;
+
+	var locationServices = {
+		computeLocation: function() {
+			if (navigator.geolocation) {
+		        navigator.geolocation.getCurrentPosition(setPosition);
+		    } else {
+		    	//set location to ucsd center
+		    }
+		},
+
+		getLocation: function() {
+			return [lat, lng];
+		},
+
+		locationReady: function() {
+			return locationReady;
+		}
+	}
+
+	function setPosition(position) {
+		lat = position.coords.latitude;
+		lng = position.coords.longitude;
+		locationReady = true;
+	}
+
+	return locationServices;
+});
+
+
+app.directive("mapHandler", function() {
+	return {
+		restrict: 'A',
+		replace: true,
+		controller: function($scope, $rootScope, notePoller, geoLocation, $timeout) {
+			$scope.currPos = ["32.879996", "-117.237046"];
+			$scope.notes = [];
+
+			geoLocation.computeLocation();
+
+			$timeout(function() {
+				if (geoLocation.locationReady()) {
+					$scope.currPos = geoLocation.getLocation();
+					updateLocation();
+					$scope.notes = notePoller.getNotes();
+				}
+
+			}, 2000);
+
+
+			$scope.map = L.map('map').setView($scope.currPos, 15);
+
+			L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+				maxZoom: 18,
+				id: 'examples.map-i875mjb7'
+			}).addTo($scope.map);
+
+
+			notePoller.startPolling();
+
+			function updateLocation() {
+				$scope.map.panTo($scope.currPos);
+				$scope.map.setZoom(17);
+			}
+		}
+	}
+});
+
+app.controller("GeonoteController", [ "$scope", 'notePoller', 'geoLocation', function($scope, notePoller, geoLocation) {
+
+
 }]);
--- hackathon/geonote/views.py
+++ hackathon/geonote/views.py
@@ -4,18 +4,20 @@ from django.http import HttpResponse, HttpResponseRedirect
 from models import *
 from django.contrib.auth import authenticate, login as auth_login, logout
 import json
+from annoying.decorators import ajax_request
 # Create your views here.
 
 
 def index(request):
     return render(request, 'index.html')
 
+@ajax_request
 def getNotes(request):
-	if request.method == "POST":
-		ne_lat = request.POST["ne_lat"]
-		ne_long = request.POST["ne_long"]
-		sw_lat = request.POST["sw_lat"]
-		sw_long = request.POST["sw_long"]
+	if request.method == "GET":
+		ne_lat = request.GET.get("ne_lat")
+		ne_long = request.GET.get("ne_long")
+		sw_lat = request.GET.get("sw_lat")
+		sw_long = request.GET.get("sw_long")
 		#notes = note.objects.filter(lat>ne_lat,lat<sw_lat,lng>ne_long,lng<sw_long)
 		# test note outputs
 		notes_output=[{"author":"hello","lat":20.11,"lng":30.11,"content":"Hola"}]
@@ -23,6 +25,9 @@ def getNotes(request):
 		#for note in notes:
 	#		notes_output.append(getNoteDict(note))
 		return HttpResponse(json.dumps(notes_output), content_type="application/json")
+
+
+
 def createNote(request):
 	if request.method == "POST":
 		content = request.POST["content"]

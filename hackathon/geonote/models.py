a3483cea1e03af72d780133fd7db84c46113c074
Corbin Lewis <corbinllewis@gmail.com>
2014-11-15 00:50:56 -0800

    Added models.note.init function

--- hackathon/geonote/models.py
+++ hackathon/geonote/models.py
@@ -9,8 +9,23 @@ class note(models.Model):
     content = models.TextField(max_length=140)
     lat = models.FloatField()
     lng = models.FloatField()
-    startTime = models.DateTimeField(auto_now_add=True)
-    expirationTime = models.IntegerField()
+    startTime = models.DateTimeField(default=datetime.now())
+    timeDelta = models.IntegerField()
+    endTime = models.DateTimeField()
+
+    def __init__(self, timeDelta=30):
+        if timeDelta != 30:
+            self.endTime = datetime.now()+timedelta(minutes=timeDelta)
+        else:
+            self.endTime = datetime.now()+timedelta(minutes=30)
+
+        self.save()
+
+
+class User(models.Model):
+    email = models.EmailField()
+    name = models.TextField()
+
 
 def getNoteDict(note):
 	return {"author":note["author"],"lat":note["lat"],"lng":note["lng"],"content":note["content"]}
